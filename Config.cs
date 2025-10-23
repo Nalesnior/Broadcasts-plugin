7c5cb3716b63538fb20a1a9bcf3c14d9abbc87fa
Naleśnior <lisquxdd@gmail.com>
2025-10-23 20:29:37 +0200

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,16 @@
+﻿using Exiled.API.Interfaces;
+
+namespace broadcast_plugin
+{
+    public class Config : IConfig
+    {
+        public string on_join_message { get; set; } = "Welcome to the server {player}!";
+        public string on_died_message { get; set; } = "You have died. Better luck next time!";
+        public string persistent_hint_text { get; set; } = "Powered by Broadcast Plugin by Naleśnior";
+        public ushort Broadcast_Duration { get; set; } = 3;
+        public float Hint_Duration { get; set; } = 1.5f;
+        public int HintVerticalPosition { get; set; } = 20;
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using MEC;
+
+namespace broadcast_plugin
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin Instance { get; private set; }
+        private CoroutineHandle hintCoroutine;
+
+        public override string Name => "Broadcast Plugin";
+        public override string Author => "Naleśnior";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(9, 6, 1);
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            RegisterEvents();
+            base.OnEnabled();
+            Log.Info("Broadcast Plugin has been enabled.");
+        }
+
+        public override void OnDisabled()
+        {
+            UnregisterEvents();
+            if (hintCoroutine.IsRunning)
+                Timing.KillCoroutines(hintCoroutine);
+            Instance = null;
+            base.OnDisabled();
+            Log.Info("Broadcast Plugin has been disabled.");
+        }
+
+        private void RegisterEvents()
+        {
+            Exiled.Events.Handlers.Player.Verified += OnPlayerJoin;
+            Exiled.Events.Handlers.Player.Died += OnPlayerDied;
+        }
+
+        private void UnregisterEvents()
+        {
+            Exiled.Events.Handlers.Player.Verified -= OnPlayerJoin;
+            Exiled.Events.Handlers.Player.Died -= OnPlayerDied;
+        }
+
+        private void OnPlayerJoin(VerifiedEventArgs ev)
+        {
+            ev.Player.Broadcast(new Exiled.API.Features.Broadcast(Config.on_join_message.Replace("{player}", ev.Player.Nickname), Config.Broadcast_Duration));
+        }
+
+        private void OnPlayerDied(DiedEventArgs ev)
+        {
+            ev.Player.Broadcast(new Exiled.API.Features.Broadcast(Config.on_died_message, Config.Broadcast_Duration));
+        }
+    }
+}
